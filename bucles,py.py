3c8e9c2adc9ba6258f1d3bc73fa8e9aa5924be40
EdwinToaquiza <edwin.toaquiza4892@utc.edu.ec>
2025-04-28 11:42:35 -0500

    Add files via upload
    hola

---
+++ bucles,py.py
@@ -0,0 +1,51 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+edad=int(input("ingrese edad"))
+print("usted a cumplido los siguientes años")
+for i in range (1,edad):
+    print(f"has cumplido {i} años")
+
+
+
+
+        
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+         
+
+
